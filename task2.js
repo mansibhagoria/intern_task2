c5fc4c186a7f5ef98fecf518128356f395cbac89
Mansi Bhagoria <mansibhagoria@gmail.com>
2024-06-27 21:30:44 +0530

    commit1

---
+++ task2.js
@@ -0,0 +1,8 @@
+function alphabetizeString(str) {
+    return str.split('').sort().join('');
+  }
+  
+  // Example usage:
+  const exampleString = 'webmaster';
+  console.log(alphabetizeString(exampleString)); // Output: 'abeemrstw'
+  
